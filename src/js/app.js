0e6731b421c24c6e3a63ebea4310dacb84f7ab26
Anton Fedorov <anton.pzkt@gmail.com>
2020-05-09 20:17:23 +0300

    added ability change icon of button, added ability switching slides by numbers and playing slides by click on the button-play

--- src/js/app.js
+++ src/js/app.js
@@ -1,109 +1,125 @@
 'use strict';
 
-const calculatorInput = document.querySelector('#calculator-input');
-const calculatorKeyboard = document.querySelector('.calculator__panel');
-const buttonMathSqrt = document.querySelector('#button-math-sqrt');
+const images = [
+	{
+		id: 0,
+		question: 'Хотите узнать, что будет с ними через 6 лет',
+		src: 'images/first-slide-500-min.jpg',
+		alt: '',
+		description: 'Нажмите Play, чтобы увидеть',
+	},
+	{
+		id: 1,
+		question: '',
+		src: 'images/second-slide-500-min.jpg',
+		alt: '',
+		description: '',
+	},
+	{
+		id: 2,
+		question: '',
+		src: 'images/third-slide-500-min.jpg',
+		alt: '',
+		description: '',
+	},
+	{
+		id: 3,
+		question: '',
+		src: 'images/fourth-slide-500-min.jpg',
+		alt: '',
+		description: '',
+	},
+	{
+		id: 4,
+		question: '',
+		src: 'images/fifth-slide-500-min.jpg',
+		alt: '',
+		description: '',
+	},
+	{
+		id: 5,
+		question: '',
+		src: 'images/sixth-slide-500-min.jpg',
+		alt: '',
+		description: '',
+	},
 
-let numberFirst;
-let numberSecond;
-let mathOperation;
-let result = '';
+];
 
-//calculatorInput.value = '0';
+const display = document.querySelector('.slider__display');
+console.log('display', display);
+const numbers = document.querySelector('.slider__numbering');
+//console.log('numbers', numbers);
+const buttonPlay = document.querySelector('.slider__button-play');
+console.log('buttonPlay', buttonPlay.innerHTML);
 
-function clearInput() {
-	numberFirst = '';
-	numberSecond = '';
-	mathOperation = '';
-	result = '';
-	return calculatorInput.value = '';
+const delay = 1000;
+
+let slideTemplate = '';
+
+function createSlide(number) {
+	//let numberSlide = number;
+
+	slideTemplate = `
+	<div class="slider__container-image">
+		<p class="slider__question">${images[number].question}</p>
+
+		<img src=${images[number].src} alt="" class="slider__image">
+
+		<p class="slider__text">${images[number].description}</p>
+	</div>
+	`;
+
+	display.innerHTML = '';
+	display.insertAdjacentHTML('beforeend', slideTemplate);
+
+	numbers.querySelector('.slider__container-number--checked').classList.remove('slider__container-number--checked');
+	numbers.querySelector(`*[data-number="${number}"]`).classList.add('slider__container-number--checked');
+};
+
+createSlide(0);
+
+function show() {
+	if (i == images.length - 1) {
+		//console.log(i);
+		createSlide(i);
+
+		buttonPlay.innerHTML = `
+		<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
+			<path d="M12 0c-3.31 0-6.291 1.353-8.459 3.522l-2.48-2.48-1.061 7.341 7.437-.966-2.489-2.488c1.808-1.808 
+			4.299-2.929 7.052-2.929 5.514 0 10 4.486 10 10s-4.486 10-10 10c-3.872 0-7.229-2.216-8.89-5.443l-1.717 
+			1.046c2.012 3.803 6.005 6.397 10.607 6.397 6.627 0 12-5.373 12-12s-5.373-12-12-12z"/>
+		</svg>`;
+
+		i = 0;
+	} else {
+		createSlide(i);
+		//console.log(i);
+		setTimeout(show, delay, i);
+		++i;
+	}
 }
-function calcMathSqrt() {
-	result = Math.sqrt(+calculatorInput.value);
-	//console.log('result', result);
-	return calculatorInput.value = result;
-}
-function calcResult() {
-	if (mathOperation === '+') {
-		result = +numberFirst + +numberSecond;
-	}
 
-	if (mathOperation === '-') {
-		result = numberFirst - numberSecond;
-		//console.log('result', result);
-	}
+let i = 1;
 
-	if (mathOperation === '×') {
-		result = numberFirst * numberSecond;
-		//console.log('result', result);
-	}
+buttonPlay.addEventListener('click', function(event) {
+	console.log('target', event.target);
+	buttonPlay.innerHTML = '';
+	buttonPlay.innerHTML = `
+	<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
+		<path d="M11 22h-4v-20h4v20zm6-20h-4v20h4v-20z"/>
+	</svg>`;
 
-	if(mathOperation === '÷') {
-		result = numberFirst / numberSecond;
-		//console.log('result', result);
-	}
+	setTimeout(show, delay, i);
+});
 
-	calculatorInput.value = String(result);
-
-	numberFirst = '';
-	numberSecond = '';
-	mathOperation = '';
-
-	return result;
-}
-function checkInputValue() {
-	if (calculatorInput.value === '' || !isFinite(calculatorInput.value)) {
-		alert('Enter a number!');
-		calculatorInput.value = '';
-
-		return true;
-	}
-
-	return false;
-}
-
-/*** Add Event Listener for Keyboard ***/
-calculatorKeyboard.addEventListener('click', function(event) {
+numbers.addEventListener('click', function(event) {
 	let target = event.target;
-	/*** Enter Numbers from Calculator Keyboard ***/
-	if (target.classList.contains('calculator__button--number')) {
-		console.log('target.innerHTML', target.innerHTML);
-		if (result != '') {
-			calculatorInput.value = target.innerHTML;
-			result = '';
-		} else {
-			calculatorInput.value += target.innerHTML;
-		}
-	}
-	/*** Calculation Math Sqrt ***/
-	if (target.classList.contains('calculator__button--math-sqrt')) {
-		if(!checkInputValue()) {
-			calcMathSqrt();
-		}
-	}
-	/*** Calculate Math Operations ***/
-	if (target.classList.contains('calculator__button--math-operation')) {
-		if(!checkInputValue()) {
-			numberFirst = calculatorInput.value;
-			calculatorInput.value = '';
-			mathOperation = target.innerHTML;
-		}
-	}
-	/*** Calculate ResultT ***/
-	if (target.classList.contains('calculator__button--equal')) {
-		if(!checkInputValue()) {
-			numberSecond = calculatorInput.value;
-			calcResult();
-		}	
-	}
-	/*** Change Number Sign ***/
-	if (target.classList.contains('calculator__button--plus-minus')) {
-		if(!checkInputValue()) {
-			calculatorInput.value = String(-1 * (+calculatorInput.value));
-		} 		
-	}
-	/*** Clear Input by AC-button ***/
-	if (target.classList.contains('calculator__button--dropping')) {
-		clearInput();
+	//console.log('target', target);
+
+	if (target.classList.contains('slider__container-number') || target.classList.contains('slider__number')) {
+		//console.log('click NUMBER');
+		
+		setTimeout(createSlide, delay, target.dataset.number);
 	}
 });
