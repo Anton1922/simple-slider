566570ef975410caf71b3f70a359f817281ec158
Anton Fedorov <anton.pzkt@gmail.com>
2020-09-24 00:25:20 +0300

    added logoPause and logoPlay for button, added functions checkNumber and replaceSlide, optimized listener of events for numbers and other smal changes and code optimization

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const display = document.querySelector('.slider__display');
-const buttonPlay = document.querySelector('.slider__button-play');
+const button = document.querySelector('.slider__button');
 const slideFirst = document.querySelector('#first-slide');
 const slideSecond = document.querySelector('#second-slide');
 const slideThird = document.querySelector('#third-slide');
@@ -10,37 +10,72 @@ const slideFifth = document.querySelector('#fifth-slide');
 const slideSixth = document.querySelector('#sixth-slide');
 const numbers = document.querySelector('.slider__numbering');
 const delay = 1500;
-const delayManual = 0;
+const delayManual = 300;
+const logoPause = `
+	<svg xmlns="http://www.w3.org/2000/svg" width="50" 
+		height="50" viewBox="0 0 24 24">
+		<path d="M11 22h-4v-20h4v20zm6-20h-4v20h4v-20z"/>
+	</svg>
+`;
+const logoPlay = `
+	<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path d="M3 22v-20l18 10-18 10z"/></svg>
+`;
+
+function replaceSlide(slideNumber) {
+	display.querySelector('.slider__container-image--active').classList.remove('slider__container-image--active');
+	display.querySelector(`*[data-slide-number="${slideNumber}"]`).classList.add('slider__container-image--active');
+}
+
+function checkNumber(target) {
+	if (target.classList.contains('slider__container-number') || target.classList.contains('slider__number')) {
+		numbers.querySelector('.slider__container-number--checked').classList.remove('slider__container-number--checked');
+		target.classList.add('slider__container-number--checked');
+	}
+}
 
 numbers.addEventListener('click', function(event) {
 	let target = event.target;
 
-	if (target.classList.contains('slider__container-number') || target.classList.contains('slider__number')) {
-		numbers.querySelector('.slider__container-number--checked').classList.remove('slider__container-number--checked');
+	checkNumber(target);
 
-		if (target.classList.contains('slider__number')) {
-				target.closest('.slider__container-number').classList.add('slider__container-number--checked');
-			} else {
-				target.classList.add('slider__container-number--checked');
-			}
-		
-		setTimeout(() => {
-			display.querySelector('.slider__container-image--active').classList.remove('slider__container-image--active');
-			display.querySelector(`*[data-slide-number="${target.dataset.number}"]`).classList.add('slider__container-image--active');
-		}, delayManual, target.dataset.number);
-	}
+	setTimeout(() => {
+		replaceSlide(target.dataset.number);
+	}, delayManual, target.dataset.number);
+	
 });
 
+function slideShow(slideNumber) {
+	let activeSlide = display.querySelector('.slider__container-image--active');
+	console.log('activeSlide', activeSlide);
 
-buttonPlay.addEventListener('click', (slide) => {
+
+
+	new Promise(resolve => {
+		setTimeout(() => {
+			slideFirst.classList.remove('slider__container-image--active');
+			numbers.querySelector('*[data-number="0"]').classList.remove('slider__container-number--checked');
+			numbers.querySelector('*[data-number="1"]').classList.add('slider__container-number--checked');
+			resolve(slideSecond.classList.add('slider__container-image--active'));
+		}, delay);
+	})
+	.then((result) => {
+		return new Promise(resolve => {
+			setTimeout(() => {
+				slideSecond.classList.remove('slider__container-image--active');
+				numbers.querySelector('*[data-number="1"]').classList.remove('slider__container-number--checked');
+				numbers.querySelector('*[data-number="2"]').classList.add('slider__container-number--checked');
+				resolve(slideThird.classList.add('slider__container-image--active'));
+			}, delay);
+		});
+	})
+}
+
+button.addEventListener('click', (slide) => {
 	numbers.querySelector('.slider__container-number--checked').classList.remove('slider__container-number--checked');
 	numbers.querySelector('*[data-number="0"]').classList.add('slider__container-number--checked');
 	display.querySelector('.slider__container-image--active').classList.remove('slider__container-image--active');
 	slideFirst.classList.add('slider__container-image--active');
-	buttonPlay.innerHTML = `
-	<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24">
-		<path d="M11 22h-4v-20h4v20zm6-20h-4v20h4v-20z"/>
-	</svg>`;
+	button.innerHTML = logoPause;
 
 	new Promise(resolve => {
 		setTimeout(() => {
@@ -96,8 +131,7 @@ buttonPlay.addEventListener('click', (slide) => {
 				numbers.querySelector('*[data-number="5"]').classList.remove('slider__container-number--checked');
 				numbers.querySelector('*[data-number="0"]').classList.add('slider__container-number--checked');
 				slideFirst.classList.add('slider__container-image--active');
-				buttonPlay.innerHTML = `
-					<svg xmlns="http://www.w3.org/2000/svg" width="50" height="50" viewBox="0 0 24 24"><path d="M3 22v-20l18 10-18 10z"/></svg>`;
+				button.innerHTML = logoPlay;
 			}, delay);
 	});
 });
